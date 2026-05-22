d48ba13c9861f87945d1301b44be7d8774958e04
Dana Paola Martínez Oliva <danna paolamar@gmail.com>
2026-05-22 04:10:20 -0500

    modifique el env

--- app/login.tsx
+++ app/login.tsx
@@ -211,7 +211,7 @@ export default function Login() {
 
       setToast({
         tipo: "success",
-        mensaje: "ヾ(≧▽≦*)o  ¡Bienvenida de vuelta! Tu sesión se inició correctamente 🌟✨",
+        mensaje: "ヾ(≧▽≦*)o  ¡Bienvenid@ de vuelta! Tu sesión se inició correctamente 🌟✨",
       });
 
       setTimeout(() => {
@@ -232,12 +232,12 @@ export default function Login() {
       ) {
         setToast({
           tipo: "error",
-          mensaje: "(╥_╥)  Hmm, no encontré esa cuenta o la contraseña no es correcta — ¿lo intentamos de nuevo? 💭",
+          mensaje: "(╥_╥)  Hmm, no encontré esa cuenta o la contraseña no es correcta,revisa la información porfavor — ¿lo intentamos denuevo? 💭",
         });
       } else {
         setToast({
           tipo: "error",
-          mensaje: `(>_<)  Algo salió mal: ${msg} — intenta en un momento 🌙`,
+          mensaje: `(>_<)  (╥_╥)  Hmm, no encontré esa cuenta o la contraseña no es correcta  ${msg} — intenta en un momento 🌙`,
         });
       }
 
